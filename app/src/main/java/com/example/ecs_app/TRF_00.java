8d09a404661f0ef551b8c58285aaad80c5855995
Carlos Cortés <carlos.cortes@alumnos.ucn.cl>
2020-01-27 10:34:28 -0300

    TRF_01 sellos creada intent sin validaciones

--- app/src/main/java/com/example/ecs_app/TRF_00.java
+++ app/src/main/java/com/example/ecs_app/TRF_00.java
@@ -2,13 +2,33 @@ package com.example.ecs_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TRF_00 extends AppCompatActivity {
+
+    private Button button_sello;
+    private Button button_lotes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trf_00);
+
+        button_sello = (Button) findViewById(R.id.button_sello);
+        button_lotes = (Button) findViewById(R.id.button_lotes);
+
+
+        button_sello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go_sello = new Intent(v.getContext(),TRF_01_sellos.class);
+                startActivity(go_sello);
+            }
+        });
+
     }
 }
