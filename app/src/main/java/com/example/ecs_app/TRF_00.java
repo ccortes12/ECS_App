c74c4d91bdcf7fca7a1ebdc45b389aa68bd8c1ff
Carlos Cortés <carlos.cortes@alumnos.ucn.cl>
2020-01-27 17:26:03 -0300

    limpiar Button OK

--- app/src/main/java/com/example/ecs_app/TRF_00.java
+++ app/src/main/java/com/example/ecs_app/TRF_00.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class TRF_00 extends AppCompatActivity {
 
     private Button button_sello;
     private Button button_lotes;
+    private Button button_limpiar;
+
+    private EditText cont,codigo,digit,csg,mar,gross,zun;
 
 
     @Override
@@ -20,6 +24,15 @@ public class TRF_00 extends AppCompatActivity {
 
         button_sello = (Button) findViewById(R.id.button_sello);
         button_lotes = (Button) findViewById(R.id.button_lotes);
+        button_limpiar = (Button) findViewById(R.id.button_clean);
+
+        cont = (EditText) findViewById(R.id.editText_cont);
+        codigo = (EditText) findViewById(R.id.editText_codigo);
+        digit = (EditText) findViewById(R.id.editText_digit);
+        csg = (EditText) findViewById(R.id.editText_csg);
+        mar = (EditText) findViewById(R.id.editText_mar);
+        gross = (EditText) findViewById(R.id.editText_gross);
+        zun = (EditText) findViewById(R.id.editText_zun);
 
 
         button_sello.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +50,22 @@ public class TRF_00 extends AppCompatActivity {
                 startActivity(to_lotes);
             }
         });
+
+        button_limpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                limpiarText();
+            }
+        });
+    }
+
+    private void limpiarText(){
+        cont.getText().clear();
+        codigo.getText().clear();
+        digit.getText().clear();
+        csg.getText().clear();
+        mar.getText().clear();
+        gross.getText().clear();
+        zun.getText().clear();
     }
 }
