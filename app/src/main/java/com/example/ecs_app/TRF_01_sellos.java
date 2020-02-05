3ef08fe934b88e24cf5f3ccf39ae6daca874f50c
Carlos Cortés <carlos.cortes@alumnos.ucn.cl>
2020-02-05 14:15:01 -0300

    APP CFS Mobile V.1.0 FINISHED

--- app/src/main/java/com/example/ecs_app/TRF_01_sellos.java
+++ app/src/main/java/com/example/ecs_app/TRF_01_sellos.java
@@ -1,6 +1,8 @@
 package com.example.ecs_app;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.example.ecs_app.Entidades.Contenedor;
@@ -14,14 +16,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.SoapFault;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpResponseException;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class TRF_01_sellos extends AppCompatActivity {
 
     private TextView textViewCont,textViewOperacion;
     private Button ingresarSello;
     private EditText sello;
-
-
+    private Contenedor contenedor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +47,7 @@ public class TRF_01_sellos extends AppCompatActivity {
         sello = (EditText) findViewById(R.id.editText_sello);
 
         Intent intent = getIntent();
-        Contenedor contenedor = (Contenedor) intent.getSerializableExtra("objContenedor");
+        contenedor = (Contenedor) intent.getSerializableExtra("objContenedor");
 
         String cadenaOperacion = contenedor.getAnnoOperacion() + " - " + contenedor.getCorOperacion();
         String cadenaContenedor = contenedor.getSigla() + "  " + contenedor.getNumero() + " - " + contenedor.getDigito();
@@ -43,6 +57,7 @@ public class TRF_01_sellos extends AppCompatActivity {
 
 
         if(contenedor.getSello().equalsIgnoreCase("0")){
+
             sello.setText("");
             sello.setCursorVisible(true);
             sello.setFocusable(true);
@@ -58,11 +73,81 @@ public class TRF_01_sellos extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Insertar el sello en el sistema
+
+                //Validar que el campo no este vacio
+
+                if(!sello.getText().toString().equalsIgnoreCase("")){
+
+                    try {
+                        String salida = new cfs_RegistraSello().execute().get();
+
+                        if(salida.equalsIgnoreCase("1")){
+                            Toast.makeText(TRF_01_sellos.this,"ERROR, No se pudo realizar la operacion",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(TRF_01_sellos.this,"EXITO",Toast.LENGTH_SHORT).show();
+                            sello.setCursorVisible(false);
+                            sello.setFocusable(false);
+                        }
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
 
-
-
     }
 
+
+    private class cfs_RegistraSello extends AsyncTask<String,Void,String>{
+        @SuppressLint("WrongThread")
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String salida = "1";
+
+            String NAMESPACE = "http://www.atiport.cl/";
+            String URL = "http://www.atiport.cl/ws_services/PRD/Torpedo.asmx";
+            String METHOD_NAME = "CFS_RegistraSello";
+            String SOAP_ACTION = "http://www.atiport.cl/CFS_RegistraSello";
+
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+
+
+
+            request.addProperty("codLugar","ANF");
+            request.addProperty("ano_operacion",contenedor.getAnnoOperacion());
+            request.addProperty("cor_operacion",contenedor.getCorOperacion());
+            request.addProperty("cor_sello",1);
+            request.addProperty("cod_sigla",contenedor.getSigla());
+            request.addProperty("cod_numero",contenedor.getNumero());
+            request.addProperty("cod_digito",contenedor.getDigito());
+            request.addProperty("des_sello",sello.getText().toString().toUpperCase());
+
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet = true;
+
+            envelope.setOutputSoapObject(request);
+
+            HttpTransportSE transport = new HttpTransportSE(URL);
+
+            try {
+                transport.call(SOAP_ACTION, envelope);
+                SoapPrimitive resultado_xml = (SoapPrimitive) envelope.getResponse();
+
+                salida = resultado_xml.getValue().toString();
+
+            } catch (HttpResponseException e) {
+                e.printStackTrace();
+            } catch (SoapFault soapFault) {
+                soapFault.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return salida;
+        }
+    }
 }
