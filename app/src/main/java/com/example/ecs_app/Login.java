f4028dd8cce7b98b48edaa0433c8108f7f7e824e
CarlosEquipo2 <ccortes.csl@gmail.com>
2021-02-10 11:33:39 -0300

    feat(Login): Modificar metodos usando el ws interface

--- app/src/main/java/com/example/ecs_app/Login.java
+++ app/src/main/java/com/example/ecs_app/Login.java
@@ -50,6 +50,8 @@ public class Login extends AppCompatActivity {
     private Spinner turno;
     private boolean pass = false;
 
+    WS_Torpedo ws = new WS_TorpedoImp();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -233,39 +235,8 @@ public class Login extends AppCompatActivity {
         @Override
         protected String doInBackground(String... strings) {
 
-            boolean resultado = false;
+            return ws.validarCredencial(strings);
 
-            String NAMESPACE = "http://www.atiport.cl/";
-            String URL = "http://www.atiport.cl/ws_services/PRD/Torpedo.asmx";
-            String METHOD_NAME = "CFS_ValidaCredencial";
-            String SOAP_ACTION = "http://www.atiport.cl/CFS_ValidaCredencial";
-
-            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-
-            request.addProperty("usuario", strings[0]);
-            request.addProperty("password", strings[1]);
-
-            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-            envelope.dotNet = true;
-
-            envelope.setOutputSoapObject(request);
-
-            HttpTransportSE transport = new HttpTransportSE(URL);
-
-            try {
-                transport.call(SOAP_ACTION, envelope);
-                SoapPrimitive respuesta = (SoapPrimitive) envelope.getResponse();
-                String estado = respuesta.toString();
-
-                return estado;
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (XmlPullParserException e) {
-                e.printStackTrace();
-            }
-
-            return null;
         }
 
     }
@@ -275,67 +246,8 @@ public class Login extends AppCompatActivity {
         @Override
         protected ArrayList<Minera> doInBackground(Void... voids) {
 
-            ArrayList<Minera> salida = new ArrayList<>();
+            return ws.ecs_listaMineras();
 
-            String NAMESPACE = "http://www.atiport.cl/";
-            String URL = "http://www.atiport.cl/ws_services/PRD/Torpedo.asmx";
-            String METHOD_NAME = "ECS_ListarMineras";
-            String SOAP_ACTION = "http://www.atiport.cl/ECS_ListarMineras";
-
-            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-
-            request.addProperty("barra", "S");
-
-            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-            envelope.dotNet = true;
-
-            envelope.setOutputSoapObject(request);
-
-            HttpTransportSE transport = new HttpTransportSE(URL);
-
-            try {
-                transport.call(SOAP_ACTION, envelope);
-                SoapObject resultado_xml = (SoapObject) envelope.getResponse();
-
-                SoapObject listTemp = (SoapObject) resultado_xml.getProperty(1);
-                SoapObject listaminera = (SoapObject) listTemp.getProperty(0);
-                SoapObject minera = (SoapObject) listTemp.getProperty(0);
-
-                int numMineras = listaminera.getPropertyCount();
-
-                for (int i = 0; i < numMineras; i++) {
-                    SoapObject mineraAux = (SoapObject) minera.getProperty(i);
-                    Minera temp = new Minera();
-                    temp.setIntRutCliente(Integer.parseInt(mineraAux.getProperty("intRutCliente").toString()));
-                    temp.setChrDvCliente(mineraAux.getProperty("chrDvCliente").toString());
-                    temp.setVchRazonSocial(mineraAux.getProperty("vchRazonSocial").toString());
-                    temp.setVchNombreFantasia(mineraAux.getProperty("vchNombreFantasia").toString());
-                    temp.setVchGiro(mineraAux.getProperty("vchGiro").toString());
-                    temp.setVchDireccion(mineraAux.getProperty("vchDireccion").toString());
-                    temp.setVchTelefonoFax(mineraAux.getProperty("vchTelefonoFax").toString());
-                    temp.setChrCodComuna(mineraAux.getProperty("chrCodComuna").toString());
-                    temp.setBigTimeStamp(Long.parseLong(mineraAux.getProperty("bigTimeStamp").toString()));
-                    temp.setIntTonMin(Float.parseFloat(mineraAux.getProperty("intTonMin").toString()));
-                    //temp.setChrTipoEmbarque(mineraAux.getProperty("chrTipoEmbarque").toString());
-                    temp.setFlgCantidadPiezas(Integer.parseInt(mineraAux.getProperty("flgCantidadPiezas").toString()));
-                    temp.setDblTarifaAlmacenaje(Float.parseFloat(mineraAux.getProperty("dblTarifaAlmacenaje").toString()));
-                    temp.setColorPatioGrafico(mineraAux.getProperty("colorPatioGrafico").toString());
-                    temp.setDblTarTonMinimo(Float.parseFloat(mineraAux.getProperty("dblTarTonMinimo").toString()));
-                    temp.setIntTonMinTurno2(Float.parseFloat(mineraAux.getProperty("intTonMinTurno2").toString()));
-
-                    salida.add(temp);
-                }
-                return salida;
-            } catch (HttpResponseException e) {
-                e.printStackTrace();
-            } catch (SoapFault soapFault) {
-                soapFault.printStackTrace();
-            } catch (XmlPullParserException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
         }
     }
 
@@ -344,54 +256,8 @@ public class Login extends AppCompatActivity {
         @Override
         protected ArrayList<Area> doInBackground(Void... voids) {
 
-            ArrayList<Area> salida = new ArrayList<>();
+            return ws.ecs_listarAreas();
 
-            String NAMESPACE = "http://www.atiport.cl/";
-            String URL = "http://www.atiport.cl/ws_services/PRD/Torpedo.asmx";
-            String METHOD_NAME = "ECS_ListarAreas";
-            String SOAP_ACTION = "http://www.atiport.cl/ECS_ListarAreas";
-
-            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-
-            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-            envelope.dotNet = true;
-
-            envelope.setOutputSoapObject(request);
-
-            HttpTransportSE transport = new HttpTransportSE(URL);
-
-            try {
-                transport.call(SOAP_ACTION, envelope);
-                SoapObject resultado_xml = (SoapObject) envelope.getResponse();
-
-                SoapObject listaAreas = (SoapObject) resultado_xml.getProperty("lstAreas");
-
-                int numAreas = listaAreas.getPropertyCount();
-
-                for (int i = 0; i < numAreas - 1; i++) {
-
-                    SoapObject areaAux = (SoapObject) listaAreas.getProperty(i);
-                    Area temp = new Area();
-                    temp.setEstado(Integer.parseInt(areaAux.getProperty("intEstado").toString()));
-                    temp.setCodArea(areaAux.getProperty("codArea").toString().trim());
-                    temp.setDesArea(areaAux.getProperty("desArea").toString().trim());
-                    temp.setNombre(areaAux.getProperty("Nombre").toString().trim());
-                    temp.setTimeStamp(Integer.parseInt(areaAux.getProperty("timeStamp").toString()));
-
-                    salida.add(temp);
-                }
-                return salida;
-
-            } catch (HttpResponseException e) {
-                e.printStackTrace();
-            } catch (SoapFault soapFault) {
-                soapFault.printStackTrace();
-            } catch (XmlPullParserException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
         }
     }
 }
